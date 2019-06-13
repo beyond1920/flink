4f6f37caaf1fdf729727d9c70557d3dd27dea7da
JingsongLi <lzljs3620320@aliyun.com>
2019-06-13 10:28:08 +0800

    [FLINK-12810][table-planner-blink] Add as, filter, where support to table api

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/api/TableImpl.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/api/TableImpl.scala
@@ -63,7 +63,9 @@ class TableImpl(val tableEnv: TableEnvironment, operationTree: QueryOperation) e
 
   override def printSchema(): Unit = ???
 
-  override def select(fields: String): Table = ???
+  override def select(fields: String): Table = {
+    select(ExpressionParser.parseExpressionList(fields).asScala: _*)
+  }
 
   override def select(fields: Expression*): Table = {
     val expressionsWithResolvedCalls = fields.map(_.accept(callResolver)).asJava
@@ -88,17 +90,30 @@ class TableImpl(val tableEnv: TableEnvironment, operationTree: QueryOperation) e
     timeAttribute: Expression,
     primaryKey: Expression): TemporalTableFunction = ???
 
-  override def as(fields: String): Table = ???
+  override def as(fields: String): Table = {
+    as(ExpressionParser.parseExpressionList(fields).asScala: _*)
+  }
 
-  override def as(fields: Expression*): Table = ???
+  override def as(fields: Expression*): Table = {
+    new TableImpl(tableEnv, operationTreeBuilder.alias(fields.asJava, operationTree))
+  }
 
-  override def filter(predicate: String): Table = ???
+  override def filter(predicate: String): Table = {
+    filter(ExpressionParser.parseExpression(predicate))
+  }
 
-  override def filter(predicate: Expression): Table = ???
+  override def filter(predicate: Expression): Table = {
+    val resolvedCallPredicate = predicate.accept(callResolver)
+    new TableImpl(tableEnv, operationTreeBuilder.filter(resolvedCallPredicate, operationTree))
+  }
 
-  override def where(predicate: String): Table = ???
+  override def where(predicate: String): Table = {
+    filter(predicate)
+  }
 
-  override def where(predicate: Expression): Table = ???
+  override def where(predicate: Expression): Table = {
+    filter(predicate)
+  }
 
   override def groupBy(fields: String): GroupedTable = ???
 
